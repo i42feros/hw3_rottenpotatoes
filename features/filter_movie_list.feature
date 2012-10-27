ccb8c79314d78f04f05d059b3164e509b84c8b18
SoniaF <i42feros@gmail.com>
2012-10-27 16:44:09 -0700

    Hw3_part2

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -23,13 +23,38 @@ Background: movies have been added to database
   
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
+  Given I check the following ratings: PG, R
+
   # enter step(s) to uncheck all other checkboxes
+  And I uncheck the following ratings: G, PG-13
+
   # enter step to "submit" the search form on the homepage
+  When I press "ratings_submit" 
+
   # enter step(s) to ensure that PG and R movies are visible
+  Then I should see the following movies: The Terminator, When Harry Met Sally, Amelie, The Incredibles, Raiders of the Lost Ark
+
   # enter step(s) to ensure that other movies are not visible
+  And I should not see the following movies: Aladdin, The Help, Chocolat, 2001: A Space Odyssey, Chicken Run
 
 Scenario: no ratings selected
-  # see assignment
+  Given I uncheck the following ratings: G, PG-13, PG, R   
+  
+  # enter step to "submit" the search form on the homepage
+  When I press "ratings_submit" 
+
+  # enter step(s) to ensure that PG and R movies are visible
+  Then I should see none of the movies
+  #Then I should not see the following movies: The Terminator, When Harry Met Sally, Amelie, The Incredibles, Raiders of the Lost Ark, Aladdin, The Help, Chocolat, 2001: A Space Odyssey, Chicken Run
 
 Scenario: all ratings selected
-  # see assignment
+  Given I check the following ratings: G, PG-13, PG, R   
+  
+  # enter step to "submit" the search form on the homepage
+  When I press "ratings_submit" 
+
+  Then I should see all of the movies
+
+  # enter step(s) to ensure that PG and R movies are visible
+  # Then I should see the following movies: The Terminator, When Harry Met Sally, Amelie, The Incredibles, Raiders of the Lost Ark, Aladdin, The Help, Chocolat, 2001: A Space Odyssey, Chicken Run
+
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,14 +1,12 @@
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies exist/ do |movies_table|
-  # movies_hash = Hash.new
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
 	  @movie = Movie.create!(movie)	  
   end
 
-  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -23,9 +21,51 @@ end
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
-
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(%r{,\s*}).each do |rating|
+    if uncheck
+      step %Q{I uncheck "ratings_#{rating}"}
+    else
+      step %Q{I check "ratings_#{rating}"}
+    end 
+  end
 end
+
+# "Then I should see the following movies: Aladdin, Amelie"
+Then /I should (not )?see the following movies: (.*)/ do |not_should, titles_list|
+  #  ensure that that e1 occurs before e2.
+  #  page.content  is the entire content of the page as a string.
+  titles_list.split(%r{,\s*}).each do |title|
+    if not_should
+      step %Q{I should not see "#{title}"}
+    else
+      step %Q{I should see "#{title}"}
+    end 
+  end
+end
+
+Then /I should see all of the movies/  do
+  db_movies = Movie.all.count
+  rows = page.all('table#movies tbody tr').count
+  
+  if rows == db_movies
+    rows == db_movies
+  else
+    assert rows == db_movies
+  end
+end
+
+Then /I should see none of the movies/  do
+  none = 0
+  rows = page.all('table#movies tbody tr').count
+  
+  if rows == none
+    rows == none
+  else
+    assert rows == none
+  end
+end
+
